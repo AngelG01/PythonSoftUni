8a6b1a6025250d70f7429bc392b7f2196f5789f8
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2022-05-23 20:14:42 +0300

    Add files via upload

---
+++ Basics/lesson2/area_of_figures.py
@@ -0,0 +1,19 @@
+from math import pi
+figure = input()
+
+if figure == "square":
+    side = float(input())
+    area = side*side
+elif figure == "rectangle":
+    side_a = float(input())
+    side_b = float(input())
+    area = side_a*side_b
+elif figure == "circle":
+    radius = float(input())
+    area = pi*radius*radius
+elif figure == "triangle":
+    side = float(input())
+    height = float(input())
+    area = 0.5*side*height
+
+print(f"{area:.3f}")
