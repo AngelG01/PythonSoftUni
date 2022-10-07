b477e27962e0a872880e97a81fa5b9fa237f7965
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2022-10-07 21:12:20 +0300

    Delete Udemy directory

--- Udemy/day_2/tip_calculator.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to tip calculator!")
-bill = float(input("What was the bill? $"))
-percentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill? "))
-
-total = bill + (bill * (percentage / 100))
-split = total / people
-
-print(f"Each person should pay: ${split:.2f}")
