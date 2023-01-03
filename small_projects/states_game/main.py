0b686dc5b0ad213e7d72bfc7edc722b65bc3ce9f
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2023-01-03 13:39:30 +0200

    Update main.py

--- small_projects/states_game/main.py
+++ small_projects/states_game/main.py
@@ -9,6 +9,7 @@ turtle.shape(image)
 
 data = pandas.read_csv('50_states.csv')
 all_states = data['state'].tolist()
+guessed_states = []
 coordinates = data.to_dict()
 
 correct_count = 0
@@ -18,7 +19,16 @@ while correct_count < 51:
     answer_state = turtle.textinput(f'{correct_count}/50 correct guesses', "What's another state's name").title()
 
     if answer_state == 'Exit':
-        exit()
+        missing_states = []
+
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv('states_to_learn.csv')
+        break
+
     if answer_state in all_states:
         state_coordinates = data[data.state == answer_state]
         timmy = turtle.Turtle()
@@ -27,6 +37,7 @@ while correct_count < 51:
         timmy.goto(int(state_coordinates.x), int(state_coordinates.y))
         timmy.write(answer_state, align='center', font=('Arial', 8, 'normal'))
         correct_count += 1
+        guessed_states.append(answer_state)
 
 
 turtle.mainloop()
