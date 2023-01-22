bcd42242deb52433a2a92615d389daaa740f837f
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2023-01-22 23:15:18 +0200

    Create maximal_sum.py

---
+++ Advanced/exercise_multidimensional_lists/maximal_sum.py
@@ -0,0 +1,19 @@
+rows, columns = [int(x) for x in input().split()]
+
+matrix = [[int(x) for x in input().split()] for _ in range(rows)]
+max_sum = float('-inf')
+biggest_matrix = []
+for row in range(rows - 2):
+    new_square = []
+    for column in range(columns - 2):
+        first_row = matrix[row][column:column + 3]
+        second_row = matrix[row + 1][column:column + 3]
+        third_row = matrix[row + 2][column:column + 3]
+
+        current_sum = sum(first_row) + sum(second_row) + sum(third_row)
+
+        if current_sum > max_sum:
+            max_sum = current_sum
+            biggest_matrix = first_row, second_row, third_row
+print(f'Sum = {max_sum}')
+[print(*biggest_matrix[row]) for row in range(3)]
