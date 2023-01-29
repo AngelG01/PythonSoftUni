4b46a17000938e8fe3556c259fb565621cb3fb32
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2023-01-29 16:11:03 +0200

    Add files via upload

---
+++ Advanced/exercise_functions_advanced/even_or_odd.py
@@ -0,0 +1,8 @@
+def even_odd(*args):
+    if args[-1] == 'even':
+        return [int(x) for x in args[:-1:] if x % 2 == 0]
+    else:
+        return [int(y) for y in args[:-1:] if y % 2 != 0]
+
+print(even_odd(1, 2, 3, 4, 5, 6, "even"))
+print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
