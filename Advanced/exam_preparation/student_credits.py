466f9e07cacdc1ec80c422335df3824aa609ee82
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2023-02-07 11:43:43 +0200

    Update student_credits.py

--- Advanced/exam_preparation/student_credits.py
+++ Advanced/exam_preparation/student_credits.py
@@ -20,18 +20,3 @@ def students_credits(*args):
     else:
         return f'Diyan needs {240 - total_credits:.1f} credits more for a diploma.' + '\n' + "\n".join(
             f"{k} - {v:.1f}" for k, v in sorted_grades)
-
-
-print(
-    students_credits(
-        "Discrete Maths-40-500-450",
-        "AI Development-20-400-400",
-        "Algorithms Advanced-50-700-630",
-        "Python Development-15-200-200",
-        "JavaScript Development-12-500-480",
-        "C++ Development-30-500-405",
-        "Game Engine Development-70-100-70",
-        "Mobile Development-25-250-225",
-        "QA-20-300-300",
-    )
-)
