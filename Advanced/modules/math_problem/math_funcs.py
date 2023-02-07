42ceade95cac431d516b11390ad83bbb9134d2ad
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2023-02-07 18:21:05 +0200

    Create math_funcs.py

---
+++ Advanced/modules/math_problem/math_funcs.py
@@ -0,0 +1,11 @@
+def math_operator(a, operator, b):
+    if operator == '+':
+        return a + b
+    elif operator == '-':
+        return a - b
+    elif operator == '*':
+        return a * b
+    elif operator == '/':
+        return a / b
+    elif operator == '^':
+        return a ** b
