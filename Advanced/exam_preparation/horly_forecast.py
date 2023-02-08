0ba0da3e188e898f4efb4897264082aa914dbb6b
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2023-02-08 22:50:23 +0200

    Add files via upload

---
+++ Advanced/exam_preparation/horly_forecast.py
@@ -0,0 +1,28 @@
+def forecast(*args):
+    data = {}
+    for curr in args:
+        location, weather = curr[0], curr[1]
+
+        if location not in data.keys():
+            data[location] = weather
+    sorted_data = sorted(data.items(),
+                         key=lambda x: (x[1], x[0]))
+
+    sunny = ''
+    cloudy = ''
+    rainy = ''
+
+    for key, value in sorted_data:
+        if value == 'Sunny':
+            sunny += f'{key} - {value}\n'
+        elif value == 'Cloudy':
+            cloudy += f'{key} - {value}\n'
+        elif value == 'Rainy':
+            rainy += f'{key} - {value}\n'
+
+    return sunny + cloudy + rainy
+
+
+print(forecast(
+    ("Tokyo", "Rainy"),
+    ("Sofia", "Rainy")))
