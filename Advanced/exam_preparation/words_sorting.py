a5992511d8624725f705979942f1a48f56f71050
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2023-02-15 19:30:19 +0200

    Add files via upload

---
+++ Advanced/exam_preparation/words_sorting.py
@@ -0,0 +1,40 @@
+def words_sorting(*args):
+    words = {}
+    total = 0
+    for word in args:
+        value = 0
+        for letter in word:
+            value += ord(letter)
+        total += value
+        words[word] = value
+    final_string = []
+    if total % 2 == 0:
+        sorted_words = sorted(words, key= lambda x: x[0])
+        for curr in sorted_words:
+            final_string.append(f'{curr} - {words[curr]}')
+        return '\n'.join(final_string)
+    else:
+        sorted_words = sorted(words, key=lambda x: -words[x])
+        for curr in sorted_words:
+            final_string.append(f'{curr} - {words[curr]}')
+        return '\n'.join(final_string)
+
+
+
+print(
+    words_sorting(
+        'escape',
+        'charm',
+        'mythology'
+  ))
+print(
+    words_sorting(
+        'escape',
+        'charm',
+        'eye'
+  ))
+print(
+    words_sorting(
+        'cacophony',
+        'accolade'
+  ))
