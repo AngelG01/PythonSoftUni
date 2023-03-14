fcd9f6f23ae5e1b4261e76a4bab37f29fff55261
AngelG01 <50680447+AngelG01@users.noreply.github.com>
2023-03-14 10:31:24 +0200

    Create kitten.py

---
+++ OOP/exrcise_polymorphism/animals/kitten.py
@@ -0,0 +1,13 @@
+from OOP.exercise_polymorphism.animals.project.cat import Cat
+
+
+class Kitten(Cat):
+    def __init__(self, name: str, age: int, gender='Female'):
+        super().__init__(name, age, gender)
+
+    def __repr__(self):
+        return f'This is {self.name}. {self.name} is a {self.age} year old {self.gender} {self.__class__.__name__}'
+
+    @staticmethod
+    def make_sound():
+        return f'Meow'
